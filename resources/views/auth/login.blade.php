b6c13212029c91e323c3fc5937d47428de9c5171
usman <usmaarn@gmail.com>
2022-11-16 06:16:43 +0100

    added login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,31 @@
-@extends('layouts.app')
+@extends("layouts.master")
 
-@section('content')
-    <div class="p-10 bg-red-500">
-        <h1 class="text-4xl">Login Page</h1>
+@section("wrapper")
+    <div class="h-screen flex items-center p-2 flex-col gap-10">
+
+        <x-navbar />
+
+        <div class="flex-grow place-self-center flex flex-col justify-center max-w-[500px] mx-auto">
+            <div class="p-5 bg-zinc-100 rounded">
+                <h1 class="text-2xl pb-5">Login</h1>
+                <form action="" class="flex flex-col gap-3">
+                    <input type="text" placeholder="username" class="rounded px-5 py-3 border-0 bg-white" />
+                    <input type="text" placeholder="username" class="rounded px-5 py-3 border-0 bg-white" />
+                    <button class="rounded px-5 py-3 border-0 bg-primary font-bold">
+                        Login <i class="bi bi-box-arrow-in-right"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
+
+        <div class="flex items-center gap-5 col-span-2 flex-wrap">
+            <a href=""
+                v-for="link in ['facebook', 'twitter', 'instagram', 'linkedin', 'youtube']"
+                class="text-zinc-400 text-xl "
+            >
+                <i :class="'bi bi-'+ link"></i>
+            </a>
+        </div>
+
     </div>
 @endsection
---
+++ resources/views/components/comming-soon.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="mt-24 py-24 px-5 font-poppins">
+        <h1 class="text-4xl font-black">This Page is under construction</h1>
+        <h3>Will be available soon</h3>
+    </div>
+
+@endsection
